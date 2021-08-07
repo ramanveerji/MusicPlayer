9a167c27f550d401ee5f204c5c8abf74679d3f51
RamandeepS <70764724+ramanveerji@users.noreply.github.com>
2021-08-07 22:50:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/subinps/MusicPlayer.git /MusicPlayer
+git clone https://github.com/ramanveerji/MusicPlayer.git /MusicPlayer
 cd /MusicPlayer
 echo "Starting Bot...."
 python3 main.py
